8ec4c4c26539e4e498572e34d72ca82ee550b9ad
moury12 <tanzibamouri00@gmail.com>
2024-10-10 14:18:52 +0600

    feat: socket initial feature

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,55 @@
+import 'package:chat_application/services/chat_service.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+ final TextEditingController messageController=TextEditingController();
+ final TextEditingController recipientController=TextEditingController();
+ @override
+  void initState() {
+    super.initState();
+    ChatService().initializeSocket();
+  }
+  @override
+  void dispose() {
+   ChatService().disconnect();
+   messageController.dispose();
+   recipientController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+  void sendMessage(){
+   String message =messageController.text;
+   String recipient = recipientController.text;
+   ChatService().sendMessage(message, recipient);
+   messageController.clear();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('data'),),
+      body:Column(children: [
+        TextField(
+          controller: recipientController,
+          decoration: InputDecoration(
+            labelText: 'Recipient Username',
+          ),
+        ),TextField(
+          controller: messageController,
+          decoration: InputDecoration(
+            labelText: 'Message',
+          ),
+        ),
+        ElevatedButton(onPressed: () {
+          sendMessage();
+        }, child:Text('send message'))
+      ],),
+    );
+  }
+}

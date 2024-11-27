ae94f38d8e0aeb402e19a9b14ef4a5e9b392d109
moury12 <tanzibamouri00@gmail.com>
2024-11-27 17:15:31 +0600

    nothing change

--- lib/main.dart
+++ lib/main.dart
@@ -8,57 +8,57 @@ import 'package:chat_application/views/auth-views/auth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:workmanager/workmanager.dart';
+// import 'package:workmanager/workmanager.dart';
 
 
-Future<void> initializeSocketInBackground() async {
+// Future<void> initializeSocketInBackground() async {
+//
+//
+//   if (Boxes.getUserToken().isNotEmpty&&Boxes.getUserToken().get('user_id')!=null&&Boxes.getUserToken().get('user_id').isNotEmpty) {
+//     final userId = Boxes.getUserToken().get('user_id');
+//
+//     // Initialize the socket with the fetched userId
+//       final socketService = SocketService(currentUserId: userId!);
+//
+//   await socketService.initializeSocket();
+//
+//   // Listen to incoming messages
+//   socketService.messageStream.listen((message) {
+//   print("Message received in background: ${message.message}");
+//
+//   // Show notification for the new message
+//   NotificationService.showNotification(
+//   1,
+//   "Message from ${message.from}",
+//   message.message??'',
+//   );
+//   });
+//   } else {
+//     print("UserInfoBloc is not in a loaded state. Cannot fetch userId.");
+//   }
+//
+//
+//
+//   // Initialize the SocketService
+//
+// }
 
-
-  if (Boxes.getUserToken().isNotEmpty&&Boxes.getUserToken().get('user_id')!=null&&Boxes.getUserToken().get('user_id').isNotEmpty) {
-    final userId = Boxes.getUserToken().get('user_id');
-
-    // Initialize the socket with the fetched userId
-      final socketService = SocketService(currentUserId: userId!);
-
-  await socketService.initializeSocket();
-
-  // Listen to incoming messages
-  socketService.messageStream.listen((message) {
-  print("Message received in background: ${message.message}");
-
-  // Show notification for the new message
-  NotificationService.showNotification(
-  1,
-  "Message from ${message.from}",
-  message.message??'',
-  );
-  });
-  } else {
-    print("UserInfoBloc is not in a loaded state. Cannot fetch userId.");
-  }
-
-
-
-  // Initialize the SocketService
-
-}
-
-@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async{
-    print("Native called background task: $task");
-    await initializeSocketInBackground();
-    //simpleTask will be emitted here.
-    return Future.value(true);
-  });
-}
+// @pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+// void callbackDispatcher() {
+//   Workmanager().executeTask((task, inputData) async{
+//     print("Native called background task: $task");
+//     await initializeSocketInBackground();
+//     //simpleTask will be emitted here.
+//     return Future.value(true);
+//   });
+// }
 void main() async{
   await Hive.initFlutter();
   await Hive.openBox("userInfo");
-  Workmanager().initialize(
-      callbackDispatcher, // The top level function, aka callbackDispatcher
-      isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-  );
+  // Workmanager().initialize(
+  //     callbackDispatcher, // The top level function, aka callbackDispatcher
+  //     isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+  // );
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService.initialize();
   runApp(const MyApp());
